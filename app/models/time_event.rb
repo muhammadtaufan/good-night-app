ab490bfb6ae16c1e17ec541eb7c56c1a8228621b
mtaufan.dev <mtaufan.dev@gmail.com>
2023-04-05 14:21:50 +0700

    add follows migration

--- app/models/time_event.rb
+++ app/models/time_event.rb
@@ -2,4 +2,8 @@ class TimeEvent < ApplicationRecord
   belongs_to :user
   validates :event_time, presence: true
   validates :is_time_in, inclusion: { in: [true, false] }
+
+  def time_out?
+    !is_time_in?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_102009) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_062228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "time_events", force: :cascade do |t|
     t.bigint "user_id", null: false
